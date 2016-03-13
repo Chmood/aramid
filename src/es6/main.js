c5ee04237311dfe8c0c87e8ba48a55ecae68fd5f
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2016-03-13 02:40:44 +0100

    Hamburger toggler activated - ES6 FTW!

---
+++ src/es6/main.js
@@ -0,0 +1,29 @@
+/*globals window, document, console */
+
+window.onload = function () {
+  'use strict';
+console.log("uhuh");
+  // TOGGLERS
+
+  var $togglers = document.querySelectorAll('[data-toggle]');
+
+  Array.prototype.forEach.call($togglers, function(el){
+
+    console.log();
+
+    var $target = document.querySelectorAll(el.getAttribute('data-toggle'))[0];
+    var classname = el.getAttribute('data-toggle-classname') || "is-collapsed";
+
+    el.addEventListener('click', function() {
+
+      if ($target.classList.contains(classname)) {
+        $target.classList.remove(classname);
+
+      } else {
+        $target.classList.add(classname);
+      }
+    });
+
+
+  });
+};
